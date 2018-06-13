e100eab6e576ea1770e45decac2e35c59e97a73e
yasui05821 <yasui05821@gmail.com>
2018-06-14 00:43:09 +0900

    求人情報ページ　作成

--- components/pages/Top/About.vue
+++ components/pages/Top/About.vue
@@ -4,7 +4,7 @@
     <img src="~assets/images/leaf6.svg" class="leaf6">
     <div class="p-about_frame"/>
     <div class="c-container">
-      <img src="~assets/images/logo.svg" class="p-about_logo">
+      <img src="~assets/images/logo.svg" class="p-about_logo" alt="Scala Kansai summit2018">
       <h3 class="p-about_heading">Scala関西Summit2018について</h3>
       <div class="p-about_read">
         Scala関西Summit はScala初心者から上級者まで楽しめる、Scalaの技術を共有するイベントです（このテキストは仮で入れています。）
